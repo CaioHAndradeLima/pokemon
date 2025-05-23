87ffae049b5a25ebebb5bea43cb7b5b17f26be30
Caio Henrique <caiohandradelima@gmail.com>
2025-05-23 16:13:22 -0300

    remove loading from request resource

--- app/src/main/java/com/example/pokemon/common/network/RequestResource.kt
+++ app/src/main/java/com/example/pokemon/common/network/RequestResource.kt
@@ -5,5 +5,4 @@ import com.example.pokemon.common.resource.UiText
 sealed class RequestResource<T>(val data: T? = null, val message: UiText? = null) {
     class Success<T>(data: T) : RequestResource<T>(data)
     class Error<T>(message: UiText, data: T? = null) : RequestResource<T>(data, message)
-    class Loading<T>(data: T? = null) : RequestResource<T>(data)
 }
