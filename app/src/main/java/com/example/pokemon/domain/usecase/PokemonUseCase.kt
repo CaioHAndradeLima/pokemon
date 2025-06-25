a534e952f601dfdd0af7e2bbfc1aa5ef3c3513d4
Caio Henrique <caiohandradelima@gmail.com>
2025-06-25 12:45:42 -0300

    update use case to handle with repository rx

--- app/src/main/java/com/example/pokemon/domain/usecase/PokemonUseCase.kt
+++ app/src/main/java/com/example/pokemon/domain/usecase/PokemonUseCase.kt
@@ -4,24 +4,23 @@ import com.example.pokemon.common.network.RequestResource
 import com.example.pokemon.data.ResponseApi
 import com.example.pokemon.data.model.Pokemon
 import com.example.pokemon.data.repository.PokemonApiRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import io.reactivex.rxjava3.core.Observable
 
 class PokemonUseCase(
     private val repository: PokemonApiRepository
 ) {
 
-    operator fun invoke(id: String): Flow<RequestResource<Pokemon>> = flow {
-        emit(RequestResource.Loading())
-
-        when (val pokemon = repository.getPokemon(id)) {
-            is ResponseApi.Success -> {
-                emit(RequestResource.Success(pokemon.data))
-            }
-
-            is ResponseApi.Error -> {
-                emit(RequestResource.Error(pokemon.message))
-            }
-        }
+    operator fun invoke(id: String): Observable<RequestResource<Pokemon>> {
+        return Observable.concat(
+            Observable.just(RequestResource.Loading()),
+            repository.getPokemon(id)
+                .map { response ->
+                    when (response) {
+                        is ResponseApi.Success -> RequestResource.Success(response.data)
+                        is ResponseApi.Error -> RequestResource.Error(response.message)
+                    }
+                }
+                .toObservable()
+        )
     }
 }
--- app/src/main/java/com/example/pokemon/domain/usecase/PokemonsUseCase.kt
+++ app/src/main/java/com/example/pokemon/domain/usecase/PokemonsUseCase.kt
@@ -1,9 +1,11 @@
 package com.example.pokemon.domain.usecase
 
 import com.example.pokemon.common.network.RequestResource
+import com.example.pokemon.common.resource.UiText
 import com.example.pokemon.data.ResponseApi
 import com.example.pokemon.data.model.Pokemon
 import com.example.pokemon.data.repository.PokemonApiRepository
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -11,22 +13,21 @@ class PokemonsUseCase(
     private val repository: PokemonApiRepository
 ) {
 
-    operator fun invoke(): Flow<RequestResource<List<Pokemon>>> = flow {
-        emit(RequestResource.Loading())
-
-        when (val pokemons = repository.getPokemons()) {
-            is ResponseApi.Success -> {
-                emit(
-                    RequestResource.Success(
-                        pokemons.data
-                        .filter { it.sprites?.hasPicture() == true }
-                        .map { it.copy() })
-                )
-            }
-
-            is ResponseApi.Error -> {
-                emit(RequestResource.Error(pokemons.message))
-            }
-        }
+    operator fun invoke(): Observable<RequestResource<List<Pokemon>>> {
+        return Observable.concat(
+            Observable.just(RequestResource.Loading()),
+            repository.getPokemons()
+                .map { response ->
+                    when (response) {
+                        is ResponseApi.Success -> RequestResource.Success(
+                            response.data
+                                .filter { it.sprites?.hasPicture() == true }
+                                .map { it.copy() }
+                        )
+                        is ResponseApi.Error -> RequestResource.Error(response.message)
+                    }
+                }
+                .toObservable()
+        )
     }
 }
